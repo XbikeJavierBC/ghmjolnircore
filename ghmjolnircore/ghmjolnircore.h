6b1e077dfc22984926a04f29015b6cd0d7caf0ab
Javier Bolaños Carapia <javier.bc121086@gmail.com>
2022-01-22 13:51:29 -0600

    Initial Commit

---
+++ ghmjolnircore/ghmjolnircore.h
@@ -0,0 +1,18 @@
+//
+//  ghmjolnircore.h
+//  ghmjolnircore
+//
+//  Created by Javier Carapia on 22/01/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ghmjolnircore.
+FOUNDATION_EXPORT double ghmjolnircoreVersionNumber;
+
+//! Project version string for ghmjolnircore.
+FOUNDATION_EXPORT const unsigned char ghmjolnircoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ghmjolnircore/PublicHeader.h>
+
+
