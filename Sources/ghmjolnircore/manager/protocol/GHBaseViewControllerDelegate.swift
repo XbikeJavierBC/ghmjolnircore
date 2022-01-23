948d025a62ebc0973e6b47ca0bdab0c3bb4b5a3d
Javier Bolaños Carapia <javier.bc121086@gmail.com>
2022-01-23 01:58:24 -0600

    Se agrega nuevo tipo devuelto al instance por storyboard

--- Sources/ghmjolnircore/manager/protocol/GHBaseViewControllerDelegate.swift
+++ Sources/ghmjolnircore/manager/protocol/GHBaseViewControllerDelegate.swift
@@ -13,17 +13,17 @@ public protocol GHBaseViewControllerDelegate: AnyObject {
     var controllerManager: GHManagerController? { get set }
     var viewModel: GHBaseViewModelProtocol? { get set }
     
-    static func instantiate(fromStoryboard nibName: String, bundle: Bundle) -> UIViewController
+    static func instantiate(fromStoryboard nibName: String, bundle: Bundle) -> Self
     
     func removeReferenceContext()
 }
 
 extension GHBaseViewControllerDelegate where Self: UIViewController {
-    public static func instantiate(fromStoryboard nibName: String, bundle: Bundle = .main) -> UIViewController {
+    public static func instantiate(fromStoryboard nibName: String, bundle: Bundle = .main) -> Self {
         let id = String(describing: self)
         let storyboard = UIStoryboard(name: nibName, bundle: bundle)
         let controller = storyboard.instantiateViewController(withIdentifier: id)
-        return controller
+        return controller as! Self
     }
     
     public func removeReferenceContext() {
